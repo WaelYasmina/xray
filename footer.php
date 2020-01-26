d0eab62daa1b422a772d6a32f7e2a1ed40e6aa78
WaelYasmina <waelyasmina@outlook.com>
2020-01-26 14:21:28 +0100

    1 episode files

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>This is the footer
+<?php wp_footer(); ?>
+</footer>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function load_scripts() {
+    wp_enqueue_style('stylesheet', get_template_directory_uri() . '/css/stylesheet.css', false, '1.0', 'all');
+
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/scripts.js', array(), false, false);
+}
+
+add_action('wp_enqueue_scripts', 'load_scripts');
+
+
+?>
